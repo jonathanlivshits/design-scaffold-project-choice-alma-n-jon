dec147feb7209f1294aad30904234223bddac388
alma1in <almal@nycstudents.net>
2022-03-16 13:11:46 -0400

    3/16/22

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-document.querySelector(".nav-item-desk-theme").addEventListener("click", function () {
-  if (document.body.classList.contains("blue-theme")) {
-    document.body.classList.add("pink-theme");
-    document.body.classList.remove("blue-theme");
-  }
-  else (document.body.classList.contains("pink-theme")) {
-    document.body.classList.add("blue-theme");
-    document.body.classList.remove("pink-theme");
-}
-});
+document
+  .querySelector(".nav-item-desk-theme")
+  .addEventListener("click", function () {
+    if (document.body.classList.contains("blue-theme")) {
+      document.body.classList.add("pink-theme");
+      document.body.classList.remove("blue-theme");
+    } else document.body.classList.contains("pink-theme");
+    {
+      document.body.classList.add("blue-theme");
+      document.body.classList.remove("pink-theme");
+    }
+  });
