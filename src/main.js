eddc13723dde222d683a9003f5864247ddb554e7
jonathanlivshits <jonathanl43@nycstudents.net>
2022-03-16 13:11:57 -0400

    3/16/22 v2

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-document.querySelector(".nav-item-desk-theme").addEventListener("click", function () {
-  if (document.body.classList.contains("blue-theme")) {
-    document.body.classList.add("pink-theme");
-    document.body.classList.remove("blue-theme");
-  }
-  else (document.body.classList.contains("pink-theme")) {
-    document.body.classList.add("blue-theme");
-    document.body.classList.remove("pink-theme");
-}
-});
+import "../style/style.css";
+
+document
+  .querySelector(".nav-item-desk-theme")
+  .addEventListener("click", function () {
+    if (document.body.classList.contains("blue-theme")) {
+      document.body.classList.add("pink-theme");
+      document.body.classList.remove("blue-theme");
+    } else if (document.body.classList.contains("pink-theme")) {
+      document.body.classList.add("blue-theme");
+      document.body.classList.remove("pink-theme");
+    }
+  });
