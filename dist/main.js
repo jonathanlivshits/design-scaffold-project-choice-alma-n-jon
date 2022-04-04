e4c474ade306057a3824a1283fd44099fe639fdc
jonathanlivshits <90789733+jonathanlivshits@users.noreply.github.com>
2022-04-04 12:51:22 -0400

    4/4/22

--- dist/main.js
+++ dist/main.js
@@ -1,16 +1,12 @@
 const themer_btn = document.getElementById("themer");
-function themeFunction() {
+function themeFunction(event) {
+  event.preventDefault();
   if (document.body.classList.contains("pink-theme")) {
     document.body.classList.remove("pink-theme");
     document.body.classList.add("blue-theme");
-    themer_btn.innerHTML = "theme: pink";
   } else {
     document.body.classList.remove("blue-theme");
     document.body.classList.add("pink-theme");
-    themer_btn.innerHTML = "theme: blue";
   }
 }
-
-/* document.getElementById("themer").addEventListener("click", themeFunction); */
-
-console.log("Hello");
+document.getElementById("themer").addEventListener("click", themeFunction);
